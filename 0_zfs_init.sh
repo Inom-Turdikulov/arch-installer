6b471119638a75d935c2947b85f4d9033c38eb6d
Inom <inomoz@users.noreply.github.com>
2021-12-09 02:39:00 +0300

    Update 0_zfs_init.sh

--- 0_zfs_init.sh
+++ 0_zfs_init.sh
@@ -1,5 +1,4 @@
 # set bigger font
 setfont latarcyrheb-sun32
-pacman -Syy
 pacman -Syy # sometimes core.db is broken, need rerun this
 curl -s https://raw.githubusercontent.com/inomoz/archiso-zfs-1/master/init | bash
