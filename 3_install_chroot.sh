850291ce7021dfd2345a5d2c3f33c1d14893744b
Inom <inomoz@users.noreply.github.com>
2021-12-09 14:19:13 +0300

    Update 3_install_chroot.sh

--- 3_install_chroot.sh
+++ 3_install_chroot.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 set -e
-echo "Remove all lines in ```/etc/fstab```, leaving only the entries for ```swap``` and ```boot```."
-sed -i '/swap\|boot\|SWAP/!d' /etc/fstab
-
-echo "Add archzfs repo"
-sed -i '/\[core\]/i[archzfs]\nServer = http://archzfs.com/$repo/x86_64\n' /etc/pacman.conf
-
-echo Add GPG keys
-curl -O https://archzfs.com/archzfs.gpg
-pacman-key -a archzfs.gpg
-pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
-pacman -Syy
 
 echo "Configure time zone (change accordingly)"
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
